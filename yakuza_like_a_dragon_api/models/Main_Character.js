5b2afe06f67662521e02a93b1ee475c38b5400ea
acanodev <aaroncanofdez@gmail.com>
2026-03-07 16:02:21 +0100

    Main Character and Sujimon models

---
+++ yakuza_like_a_dragon_api/models/Main_Character.js
@@ -0,0 +1,27 @@
+const { Schema, model } = require("mongoose");
+
+const mainCharacterSchema = new Schema({
+    name:
+    {
+        type: String,
+        required: true
+    },
+    jobs:
+    {
+        type: [String],
+        required: true
+    },
+    image:
+    {
+        type: String,
+        default: null
+    },
+    description:
+    {
+        type: String,
+        default: null
+    }
+})
+
+const mainCharacter = model('mainCharacter', mainCharacterSchema);
+module.exports = mainCharacter;
---
+++ yakuza_like_a_dragon_api/models/Sujimon.js
@@ -0,0 +1,37 @@
+const { Schema, model } = require("mongoose");
+
+const sujimonSchema = new Schema({
+    name:
+    {
+        type: String,
+        required: true
+    },
+    common_locations:
+    {
+        type: [String],
+        required: true
+    },
+    rarity:
+    {
+        type: Number,
+        required: true
+    },
+    weaknesses:
+    {
+        type: [String],
+        required: true
+    },
+    image:
+    {
+        type: String,
+        default: null
+    },
+    description:
+    {
+        type: String,
+        default: null
+    },
+})
+
+const Sujimon = model('Sujimon', sujimonSchema);
+module.exports = Sujimon;
