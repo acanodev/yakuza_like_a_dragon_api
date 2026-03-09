fc55be4a2c87c54fa804f64952043fb4863108f5
Aarón Cano <aaron_canofernandez@iescarlesvallbona.cat>
2026-03-09 20:18:06 +0100

    Font Awesome installed and imported

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -4,6 +4,7 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap/dist/js/bootstrap.js'
 import 'bootstrap/dist/js/bootstrap.esm.js'
 import 'bootstrap/dist/js/bootstrap.bundle.js'
+import '@fortawesome/fontawesome-free/css/all.min.css'
 import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
