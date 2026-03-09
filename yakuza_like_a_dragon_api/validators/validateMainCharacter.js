fe837387768dd18df0c90cf9345f8e648cd03214
Aarón Cano <aaron_canofernandez@iescarlesvallbona.cat>
2026-03-09 19:40:00 +0100

    Validate unique id_num in main characters and sujimon

--- yakuza_like_a_dragon_api/validators/validateMainCharacter.js
+++ yakuza_like_a_dragon_api/validators/validateMainCharacter.js
@@ -1,10 +1,19 @@
-module.exports = (data) => {
+const mainCharacter = require("../models/Main_Character");
+
+module.exports = async (data) => {
+  const mcs = await mainCharacter.find({});
+  const ids_num = mcs.map((el) => el.id_num);
+
   if (!data.name || typeof data.name !== "string") {
     return "Name must be a string";
   }
 
   if (isNaN(data.id_num) || data.id_num < 1) {
     return "id_num must be a number greater than 0";
+  }
+
+  if (ids_num.includes(Number(data.id_num))) {
+    return "id_num must be unique";
   }
 
   return null;
