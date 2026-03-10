fccc087de9c9a7d54f8cc30f143d7ce55b1cac55
acanodev <aaroncanofdez@gmail.com>
2026-03-10 12:23:25 +0100

    Zod Schema for Sujimon

---
+++ frontend/src/schemas/sujimon.ts
@@ -0,0 +1,26 @@
+import { z } from "zod";
+
+/* 
+Com els sujimon tenen més d'un camp de tipus array,
+fem un esquema general per a transformar els strings 
+separats per ","
+*/
+const arraySchema = z
+  .string()
+  .min(1)
+  .transform((val) => 
+    val.split(",").map((el) => el.trim())
+  );
+
+export const sujimonSchema = z.object({
+    name: z.string().min(1),
+    id_num: z.number().int().positive(),
+    category: z.string().min(1),
+    common_locations: arraySchema,
+    rarity: z.number().int().min(1).max(5),
+    skills: arraySchema,
+    weaknesses: arraySchema,
+    drops: arraySchema,
+    image: z.string().url().optional(),
+    description: z.string().min(1).optional(),
+});
