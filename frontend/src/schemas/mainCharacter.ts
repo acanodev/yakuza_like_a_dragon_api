b3c6ae8a2d82da9ea96c4415541f9c9a2248f966
Aarón Cano <aaron_canofernandez@iescarlesvallbona.cat>
2026-03-10 12:06:52 +0100

    Zod Schema for mainCharacter

---
+++ frontend/src/schemas/mainCharacter.ts
@@ -0,0 +1,17 @@
+import { z } from "zod";
+
+// Conversió d'string separat per "," que arriba des del formulari a array d'strings
+const jobsSchema = z
+  .string()
+  .min(1)
+  .transform((val) =>
+    val.split(",").map((job) => job.trim())
+  );
+
+export const newMainCharacterSchema = z.object({
+  name: z.string().min(1),
+  id_num: z.number().int().positive(),
+  jobs: jobsSchema, // A l'schema utilitzem jobSchema per validar la conversió directament.
+  image: z.string().url().optional(),
+  description: z.string().min(1).optional(),
+});
