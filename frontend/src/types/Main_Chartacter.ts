1befb16999ad1957f68c76168a2269ca6a0c3221
Aarón Cano <aaron_canofernandez@iescarlesvallbona.cat>
2026-03-10 11:39:08 +0100

    Main Character type

---
+++ frontend/src/types/Main_Chartacter.ts
@@ -0,0 +1,23 @@
+export type MainCharacter = {
+  _id: string;
+  name: string;
+  id_num: number;
+  jobs: string[];
+  image: string;
+  description: string;
+};
+
+export type NewMainCharacter = {
+  name: string;
+  id_num: number;
+  jobs: string[];
+  image: string;
+  description: string;
+};
+
+export type MainCharacterFormProps = {
+  newContent: NewMainCharacter;
+  editingNote: MainCharacter | null;
+  onChange: (content: NewMainCharacter) => void;
+  onSubmit: (e: React.FormEvent<HTMLFormElement>) => void;
+};
