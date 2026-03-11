7a13f39bfe3b9e16d9220493073a1f25146c13f6
acanodev <aaroncanofdez@gmail.com>
2026-03-11 11:18:38 +0100

    Changed headerText and id alternative type value to undefined

--- frontend/src/components/Card.tsx
+++ frontend/src/components/Card.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-function Card({ headerText = null, children, id = null } : { headerText : string | null, children : React.ReactNode, id : any }) {
+function Card({ headerText = undefined, children, id = undefined } : { headerText : string | undefined, children : React.ReactNode, id : any }) {
   const className = `card mb-4`;
 
   return (
