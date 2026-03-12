6079f1d14ec8b882633fa4150468782b7108a963
Aarón Cano <aaron_canofernandez@iescarlesvallbona.cat>
2026-03-12 19:33:52 +0100

    MainCharacterForm component (NOT UPLOADED ON LAST COMMIT)

---
+++ frontend/src/components/MainCharacterForm/MainCharacterForm.tsx
@@ -0,0 +1,67 @@
+import "./MainCharacterForm.css";
+import Form from "../Form";
+import Input from "../Input";
+import Textarea from "../Textarea";
+import Modal from "../Modal";
+import Button from "../Button";
+import type { z } from "zod";
+import { mainCharacterSchema } from "../../schemas/mainCharacter";
+import type { MainCharacter } from "../../types/Main_Chartacter";
+
+type MainCharacterFormData = z.input<typeof mainCharacterSchema>;
+
+type MainCharacterFormProps = {
+  show: boolean;
+  data?: MainCharacter;
+  submitHandler: (data: MainCharacterFormData) => void;
+  toggleShow: () => void;
+};
+
+function MainCharacterForm({ show, data, submitHandler, toggleShow }: MainCharacterFormProps) {
+  return (
+    <Modal
+      show={show}
+      title={data ? `Editar ${data.name}` : "Crear Main Character"}
+      submitButton={false}
+      toggleShow={toggleShow}
+    >
+      <Form
+        id="mainCharacterForm"
+        schema="mainCharacter"
+        submitHandler={submitHandler as any}
+        bootstrap="d-flex flex-column gap-3"
+      >
+        <Input bootstrap="form-control" type="text" name="name" id="name">
+          Name
+        </Input>
+
+        <Input bootstrap="form-control" type="number" name="id_num" id="id_num">
+          ID
+        </Input>
+
+        <Input bootstrap="form-control" type="text" name="jobs" id="jobs">
+          Treballs ("," separated)
+        </Input>
+
+        <Input bootstrap="form-control" type="text" name="image" id="image">
+          Image URL
+        </Input>
+
+        <Textarea
+          bootstrap="form-control"
+          name="description"
+          id="description"
+          rows={4}
+        >
+          Descripció
+        </Textarea>
+
+        <Button bootstrap="btn btn-success" type="submit">
+          {!data ? "Afegir" : "Editar"}
+        </Button>
+      </Form>
+    </Modal>
+  );
+}
+
+export default MainCharacterForm;
