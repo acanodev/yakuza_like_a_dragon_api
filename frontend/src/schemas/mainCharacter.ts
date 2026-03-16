437f5fd82604b06cf2d7e0a2f970ece08cf817e0
acanodev <aaroncanofdez@gmail.com>
2026-03-16 11:51:31 +0100

    Extra validation for Main Character name. Name trim can't be an empty space.

--- frontend/src/schemas/mainCharacter.ts
+++ frontend/src/schemas/mainCharacter.ts
@@ -1,9 +1,12 @@
 import { z } from "zod";
 
 export const mainCharacterSchema = z.object({
-  name: z.string().min(1),
+  name: z.string().min(1).transform((val) => (val.trim() === "" ? undefined : val)),
   id_num: z.coerce.number().int().positive(),
-  jobs: z.string().optional().transform(val => val?.trim() === "" ? undefined : val),
+  jobs: z
+    .string()
+    .optional()
+    .transform((val) => (val?.trim() === "" ? undefined : val)),
   image: z
     .string()
     .optional()
